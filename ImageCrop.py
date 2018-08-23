d67bdabe39ab5ac1bfaf3bea4fa78709abeb1cd7
Aditya Chaurasia <olavian2014@gmail.com>
2018-08-23 12:37:47 +0100

    Update ImageCrop.py

--- ImageCrop.py
+++ ImageCrop.py
@@ -7,7 +7,7 @@ width, height = list(map(int, input("Width, Height:\t").split()))
 def cropImage(name, startX, startY, width, height):
 
     if __name__ == '__main__':
-        img = Image.open('C:/Adi/Programming/AdiPython/AdiScripts/Others/Art/InputImages/' + name)
+        img = Image.open(YOURFILEPATH + name)
     else:
         img = Image.open(name)
 
@@ -29,7 +29,7 @@ def cropImage(name, startX, startY, width, height):
     new.putdata(newData)
 
     if __name__ == '__main__':
-        new.save('C:/Adi/Programming/AdiPython/AdiScripts/Others/Art/GeneratedImages/' + name[: name.index('.')] + "Crop" + str(startX) + str(startY) + str(width) + str(height) + '.png')
+        new.save(YOURFILEPATH + name[: name.index('.')] + "Crop" + str(startX) + str(startY) + str(width) + str(height) + '.png')
     else:
         dest = input("Destination:\t")
         new.save(dest + '.png')
